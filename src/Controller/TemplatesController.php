adeff4daa43bf7bafd2c74af1800f083413bdb0e
Grzegab <greg@grzegab.eu>
2019-09-30 23:21:23 +0200

    - add templates - updated docker config - updated nginx config - add assets

--- src/Controller/TemplatesController.php
+++ src/Controller/TemplatesController.php
@@ -41,4 +41,91 @@ class TemplatesController
         return new Response($this->twig->render('templates/photo-gallery.html.twig', ['faker' => $faker]));
     }
 
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function videoGallery(): Response
+    {
+        $faker = new Generator();
+        $faker->addProvider(new Lorem($faker));
+
+        return new Response($this->twig->render('templates/video-gallery.html.twig', ['faker' => $faker]));
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function mixedGallery(): Response
+    {
+        $faker = new Generator();
+        $faker->addProvider(new Lorem($faker));
+        $faker->addProvider(new Image($faker));
+        $faker->addProvider(new DateTime($faker));
+        $faker->addProvider(new Person($faker));
+
+        return new Response($this->twig->render('templates/mixed-gallery.html.twig', ['faker' => $faker]));
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function login(): Response
+    {
+        return new Response($this->twig->render('templates/page-login.html.twig'));
+    }
+
+    public function contact(): Response
+    {
+        return new Response($this->twig->render('templates/contact.html.twig'));
+    }
+
+    public function footer(): Response
+    {
+        return new Response($this->twig->render('templates/footer.html.twig'));
+    }
+
+    public function sitemap(): Response
+    {
+        return new Response($this->twig->render('templates/sitemap.html.twig'));
+    }
+
+    public function shortcuts(): Response
+    {
+        return new Response($this->twig->render('templates/shortcuts.html.twig'));
+    }
+
+    public function faq(): Response
+    {
+        return new Response($this->twig->render('templates/faq.html.twig'));
+    }
+
+    public function thanks(): Response
+    {
+        return new Response($this->twig->render('templates/thanks.html.twig'));
+    }
+
+    public function employees(): Response
+    {
+        return new Response($this->twig->render('templates/employees.html.twig'));
+    }
+
+    public function pricing(): Response
+    {
+        return new Response($this->twig->render('templates/pricing.html.twig'));
+    }
+
+    public function createAccount(): Response
+    {
+        return new Response($this->twig->render('templates/create-account.html.twig'));
+    }
+
 }
