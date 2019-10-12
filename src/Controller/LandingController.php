c71883d3a0ce8bcc79e797558fbe403684d18cee
Grzegab <greg@grzegab.eu>
2019-10-12 14:47:11 +0200

    add routing and controllers for pages with index page

---
+++ src/Controller/LandingController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class LandingController
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function index(): Response
+    {
+        return new Response($this->twig->render('landing_page/index.html.twig'));
+    }
+}
---
+++ src/Controller/ResumeController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class ResumeController
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function index(): Response
+    {
+        return new Response($this->twig->render('resume/index.html.twig'));
+    }
+}
