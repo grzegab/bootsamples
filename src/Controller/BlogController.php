1ccd918efb67baf5fee27e0937ad1b874b25b70c
Grzegab <greg@grzegab.eu>
2019-12-29 20:04:52 +0100

    Add new pages

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -32,4 +32,59 @@ class BlogController
     {
         return new Response($this->twig->render('blog/index.html.twig'));
     }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function postVideo(): Response
+    {
+        return new Response($this->twig->render('blog/blog-post-video.html.twig'));
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function postAudio(): Response
+    {
+        return new Response($this->twig->render('blog/blog-post-audio.html.twig'));
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function postText(): Response
+    {
+        return new Response($this->twig->render('blog/blog-post-text.html.twig'));
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function postGallery(): Response
+    {
+        return new Response($this->twig->render('blog/blog-post-photo-gallery.html.twig'));
+    }
+
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function postGrid(): Response
+    {
+        return new Response($this->twig->render('blog/blog-overview-grid.html.twig'));
+    }
 }
--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -32,4 +32,44 @@ class ForumController
     {
         return new Response($this->twig->render('forum/index.html.twig'));
     }
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function Post(): Response
+    {
+        return new Response($this->twig->render('forum/post.html.twig'));
+    }
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function topicOverview(): Response
+    {
+        return new Response($this->twig->render('forum/overview-topics.html.twig'));
+    }
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function categoryOverview(): Response
+    {
+        return new Response($this->twig->render('forum/overview-forum-category.html.twig'));
+    }
+    /**
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function newTopic(): Response
+    {
+        return new Response($this->twig->render('forum/new-topic.html.twig'));
+    }
 }
